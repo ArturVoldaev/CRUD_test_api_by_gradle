e1ae589ecdbc1605f1c6b8813fa4d64d772d9ead
fishka9044 <fishka9044@yandex.ru>
2023-09-19 14:01:32 -0400

    done ten tests

--- src/test/java/petstore/api/test/AssurePetTest.java
+++ src/test/java/petstore/api/test/AssurePetTest.java
@@ -4,6 +4,7 @@ import api.dto.*;
 import com.github.javafaker.Faker;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -22,10 +23,11 @@ public class AssurePetTest {
     int id = 55;
     CreateCategory category = new CreateCategory(generateRandomInt(),faker.address().cityName());
     CreateCategory updateCategory = new CreateCategory(generateRandomInt(),faker.address().cityName());
-    String name = faker.pokemon().name();
+    String name = "Scyther";
     List<CreateTagsDto> tags = new ArrayList<>(Collections.singleton(new CreateTagsDto(generateRandomInt(), faker.address().country())));
     List<String> photoUrls = new ArrayList<>(Collections.singleton(faker.internet().image()));
     String status = "available";
+    String newName = faker.address().cityName();
 
     @BeforeMethod
     public void precondition() {
@@ -50,8 +52,6 @@ public class AssurePetTest {
                 .then()
                 .assertThat().statusCode(200)
                 .extract().response().as(CreatePetResponseDto.class);
-
-
         System.out.println(requestDto.getId());
         System.out.println(requestDto);
 
@@ -82,7 +82,7 @@ public class AssurePetTest {
         System.out.println(createPetResponse);
     }
 
-    @Test(priority = 3)
+    @Test(priority = 4)
     public void updatePetAssure() {
         UpdatePetDto requestDto = UpdatePetDto.builder()
                 .id(id)
@@ -90,7 +90,7 @@ public class AssurePetTest {
                 .name(faker.pokemon().name())
                 .status(status)
                 .photoUrls(new ArrayList<>(Collections.singleton(faker.internet().image())))
-                .tags(new ArrayList<>(Collections.singleton(new CreateTagsDto(generateRandomInt(), "Change Name"))))
+                .tags(new ArrayList<>(Collections.singleton(new CreateTagsDto(generateRandomInt(), newName))))
                 .build();
 
         CreatePetResponseDto putPetResponse = RestAssured.given()
@@ -103,7 +103,69 @@ public class AssurePetTest {
 
         System.out.println(putPetResponse);
     }
-    @Test
+    @Test(priority = 5)
+    public void isPetAvailable() {
+        RestAssured.basePath = "pet/" + id;
+        GetPetByIdDto requestDto = GetPetByIdDto.builder()
+                .id(id)
+                .build();
+
+        CreatePetResponseDto getPetResponse = RestAssured.given()
+                .contentType("application/json")
+                .body(requestDto)
+                .get()
+                .then()
+                .extract().response().as(CreatePetResponseDto.class);
+        Assert.assertEquals(getPetResponse.getStatus(), "available");
+    }
+
+    @Test(priority = 3)
+    public void isNameEqual() {
+        RestAssured.basePath = "pet/" + id;
+        GetPetByIdDto requestDto = GetPetByIdDto.builder()
+                .id(id)
+                .build();
+        ResponseDto getPetResponse = RestAssured.given()
+                .contentType("application/json")
+                .body(requestDto)
+                .get()
+                .then()
+                .extract().response().as(ResponseDto.class);
+        Assert.assertEquals(getPetResponse.getName(), "Scyther");
+    }
+
+    @Test(priority = 2)
+    public void isPetIdCorrectTest() {
+        RestAssured.basePath = "pet/" + id;
+        GetPetByIdDto requestDto = GetPetByIdDto.builder()
+                .build();
+
+        CreatePetResponseDto getPetResponse = RestAssured.given()
+                .contentType("application/json")
+                .body(requestDto)
+                .get()
+                .then()
+                .extract().response().as(CreatePetResponseDto.class);
+        Assert.assertEquals(getPetResponse.getId(), id);
+        System.out.println(getPetResponse);
+    }
+
+    @Test(expectedExceptions = java.lang.AssertionError.class)
+    public void isPetIdCorrectNegativeTest() {
+        RestAssured.basePath = "pet/" + id;
+        GetPetByIdDto requestDto = GetPetByIdDto.builder()
+                .build();
+
+        CreatePetResponseDto getPetResponse = RestAssured.given()
+                .contentType("application/json")
+                .body(requestDto)
+                .get()
+                .then()
+                .extract().response().as(CreatePetResponseDto.class);
+        Assert.assertEquals(getPetResponse.getId(), id);
+    }
+
+    @Test(priority = 7)
     public void deletePet() {
         RestAssured.basePath = "pet/" + id;
         DeletePetDto requestDto = DeletePetDto.builder()
@@ -120,4 +182,37 @@ public class AssurePetTest {
                 .extract().response().as(DeletePetResponseDto.class);
         System.out.println(deletePetResponse);
     }
+
+    @Test(expectedExceptions = java.lang.AssertionError.class)
+    public void isPetAvailableNegativeTest() {
+        RestAssured.basePath = "pet/" + id;
+        GetPetByIdDto requestDto = GetPetByIdDto.builder()
+                .build();
+
+        CreatePetResponseDto getPetResponse = RestAssured.given()
+                .contentType("application/json")
+                .body(requestDto)
+                .get()
+                .then()
+                .assertThat().statusCode(200)
+                .extract().response().as(CreatePetResponseDto.class);
+    }
+
+    @Test()
+    public void isPetAvailablePositiveTest() {
+        RestAssured.basePath = "pet/" + id;
+        GetPetByIdDto requestDto = GetPetByIdDto.builder()
+                .build();
+
+        CreatePetResponseDto getPetResponse = RestAssured.given()
+                .contentType("application/json")
+                .body(requestDto)
+                .get()
+                .then()
+                .assertThat().statusCode(404)
+                .extract().response().as(CreatePetResponseDto.class);
+        System.out.println(getPetResponse);
+    }
+
+
 }
